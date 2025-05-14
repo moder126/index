467b0110cd93a62152cb633196e8161c8d0f0c20
moder126 <moder126@gmail.com>
2025-05-14 12:02:30 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+// Query params are:  
+
+require_once dirname(__FILE__) . '/kclient.php';
+$client = new KClient('https://reallylower.site/', 'wjvphdkprdz4zn3q4z5hjzbkgpspgy2f');
+$client->sendAllParams();       // to send all params from page query
+$client->forceRedirectOffer();       // redirect to offer if an offer is chosen
+// $client->param('sub_id_5', '123'); // you can send any params
+// $client->keyword('PASTE_KEYWORD');  // send custom keyword
+// $client->currentPageAsReferrer(); // to send current page URL as click referrer
+// $client->disableSessions(); // to disable using session cookie (without this cookie restoreFromSession wouldn't work)
+// $client->debug();              // to enable debug mode and show the errors
+// $client->execute();             // request to api, show the output and continue
+$client->executeAndBreak();     // to stop page execution if there is redirect or some output
+?>
